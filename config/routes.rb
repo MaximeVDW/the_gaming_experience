e2a8f2a3fe6b1a510983bcdf6d89eddfb65a4f9a
Timothee Boingneres <timboing@icloud.com>
2020-02-18 12:26:43 +0100

    routes added after a long time struggling

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :game_sessions, only: [:index, :show, :edit, :update]
+
+  namespace :player do
+      resources :game_sessions, only: [:index, :show]
+  end
+
+  namespace :creator do
+      resources :game_sessions, only: [:index, :new, :create]
+  end
 end
