3d546b42531acb19a53a1d272128427de4e5a9c1
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-02-19 10:48:22 +0100

    add photo for user and game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_171017) do
+ActiveRecord::Schema.define(version: 2020_02_19_094653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_171017) do
     t.integer "max_player"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
@@ -87,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_171017) do
     t.string "street_number"
     t.string "postal_code"
     t.string "city"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
