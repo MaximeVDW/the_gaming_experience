eb3b96038ccfbedf25e0d735dabe2b5921aac81a
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-02-19 14:26:37 +0100

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ user4 = User.create!(email: "ddddd@ddddd.fr",password: "123456", city: "paris")
 user5 = User.create!(email: "eeeee@eeeee.fr",password: "123456", city: "paris")
 puts "Some users have been created"
 puts "Creating new games..."
-game1 = Game.create!(name: "Poker", category: "Jeu de Carte", description: "Un jeux où il faut miser", min_player: 2, max_player: 16)
-game2 = Game.create!(name: "Monopoly", category: "Plateau", description: "Un jeux où il faut plumer tes adversaires", min_player: 2, max_player: 8)
-game3 = Game.create!(name: "Risk", category: "Plateau", description: "Pars à la conquete du monde", min_player: 2, max_player: 6)
-game4 = Game.create!(name: "La bonne paye", category: "Plateau", description: "Paye, paye, paye... ou deviens riche", min_player: 2, max_player: 6)
-game5 = Game.create!(name: "Tarot", category: "Jeu de Carte", description: "Une legende de jeu de carte", min_player: 3, max_player: 5)
+game1 = Game.create!(name: "Poker", category: "Jeu de Carte", description: "Poker is a family of card games that combines gambling, strategy and different skills. All poker variants involve betting as an intrinsic part of play, and determine the winner of each hand according to the combinations of players' cards, at least some of which remain hidden until the end of the hand.", min_player: 2, max_player: 16)
+game2 = Game.create!(name: "Monopoly", category: "Plateau", description: "Monopoly is a board game currently published by Hasbro. In the game, players roll two six-sided dice to move around the game board, buying and trading properties, and developing them with houses and hotels. Players collect rent from their opponents, with the goal being to drive them into bankruptcy.", min_player: 2, max_player: 8)
+game3 = Game.create!(name: "Risk", category: "Plateau", description: "Risk is a strategy board game of diplomacy, conflict and conquest[1] for two to six players. The standard version is played on a board depicting a political map of Earth, divided into forty-two territories, which are grouped into six continents.", min_player: 2, max_player: 6)
+game4 = Game.create!(name: "La bonne paye", category: "Plateau", description: "Les joueurs jettent un dé à tour de rôle pour se déplacer sur le parcours. En fonction de la case où ils tombent, ils doivent tirer une carte, payer ou recevoir de l'argent.", min_player: 2, max_player: 6)
+game5 = Game.create!(name: "Tarot", category: "Jeu de Carte", description: "The French game of tarot, also jeu de tarot, is a trick-taking strategy tarot card game played by three to five players using a traditional 78-card tarot deck. The game is the second most popular card game in France.", min_player: 3, max_player: 5)
 puts "Some games have been created"
 puts "Creating new game sessions..."
 GameSession.create!(game: game1, creator: user1, city: "Bordeaux", date: Time.now + (2*7*24*60*60), price: 10)
