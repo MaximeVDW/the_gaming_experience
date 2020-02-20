fd161b04cbad968b0e5430819ae53abae63d8006
Timothee Boingneres <timboing@icloud.com>
2020-02-20 16:03:18 +0100

    map hide and show added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initMapToggler } from '../packs/game_sessions/index';
 initMapbox();
+initMapToggler();
